559a5aabb142e4e6d6c8f4e441735a4fe0b0507b
Jonathan Samudio <jonathan2457@gmail.com>
2017-10-03 17:05:51 -0400

    Removed pods folder from the repo.

--- Example/Pods/Target Support Files/Pilas/Pilas-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pilas : NSObject
-@end
-@implementation PodsDummy_Pilas
-@end
--- Example/Pods/Target Support Files/Pods-Pilas_Example/Pods-Pilas_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pilas_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Pilas_Example
-@end
